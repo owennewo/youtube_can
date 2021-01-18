a9423f18cb5fd437ffd9ab1f720d0176362cc537
Owen Williams <williams.owen@gmail.com>
2021-01-18 10:41:37 +0000

    adding esp32 and stm32 (canfd)

---
+++ can_esp32/src/main.cpp
@@ -0,0 +1,53 @@
+#include <Arduino.h>
+#include <CAN.h>
+
+// default pins crx=4 ctx=5
+
+void onReceive(int packetSize) {
+  // received a packet
+  Serial.print("Received packet, id=0x");
+  Serial.print(CAN.packetId(), HEX);
+
+  static int led_state = HIGH;
+  led_state = !led_state;
+  digitalWrite(BUILTIN_LED, led_state);
+
+  if (CAN.packetRtr()) {
+    Serial.print(" and requested length ");
+    Serial.println(CAN.packetDlc());
+  } else {
+    Serial.print(", length=");
+    Serial.print(packetSize);
+
+    int byte_index=0;
+    // only print packet data for non-RTR packets
+    while (CAN.available()) {
+      Serial.print(" byte["); Serial.print(byte_index); Serial.print("]=");  Serial.print(CAN.read()); Serial.print(" ");
+      byte_index++;
+    }
+    Serial.println();
+  }
+
+}
+
+void setup() {
+
+  pinMode(BUILTIN_LED, OUTPUT);
+
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println("Setup");
+
+  if (!CAN.begin(1000E3)) {
+    Serial.println("Starting CAN failed!");
+    while (1);
+  }
+
+  // register the receive callback
+  CAN.onReceive(onReceive);
+  
+}
+
+void loop() {
+
+}
